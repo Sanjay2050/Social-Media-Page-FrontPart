79c92fef10ec2272de948bb3f77ee2d057a2aaea
Sanjay2050 <sanjayshrestha2050@gmail.com>
2021-07-23 19:09:06 +1000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from "./pages/home/Home.jsx";
+import Login from "./pages/login/Login.jsx";
+import Profile from "./pages/profile/profile.jsx";
+import Register from "./pages/register/Register.jsx";
+function App() {
+  
+    
+     return <Profile />
+     
+        
+      
+  
+}
+
+export default App;
